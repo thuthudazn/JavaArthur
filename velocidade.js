752ec64b78b79db1406687d9cad52f2325b698e7
thuthudazn <113353214+thuthudazn@users.noreply.github.com>
2023-10-16 11:56:58 -0300

    16/10

--- velocidade.js
+++ velocidade.js
@@ -1,30 +1,23 @@
-//cria referencia ao form e elementos de resposta do programa
-
 const form = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+const resp = document.querySelector("h3")
 
-//cria um ouvinte
+//ouvinte
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()        //evita o envio do form
+form.addEventListener("submit", (e) =>{
+    e.preventDefault
 
-    const nome = form.inNome.value   //obtem o nome
-    const nota1 = Number(form.inNota1.value) //obtem a primeira nota
-    const nota2 = Number(form.inNota2.value) //obtem a segunda nota
-    const media = (nota1 + nota2)/2   //media das notas
-    resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
 
-    //cria condicionais
+    if(condutor <= leve) {
+        resp.innerText = 'Situação: Sem Multa'
 
-    if(media >= 7){
-        resp2.innerText = `Parabéns ${nome}! Você foi aprovado(a)`
-        resp2.style.color = "blue"
-    } else if (media >=4) {
-        resp2.innerText = `Atenção ${nome}! Você está de exame`
-        resp2.style.color = "green"
-    } else{
-        resp2.innerText = `Ops ${nome}! Você foi reprovado(a)`
-        resp2.style.color = "red"
+    } else if(condutor <= leve) {
+        resp.innerText = 'Situação: Multa Leve'
+
+    } else {  
+        resp.innerText = 'Situação Multa grave'
+
     }
 })
