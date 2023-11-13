690da367285f5eb075eec2283d64ef3d48174e2d
thuthudazn <113353214+thuthudazn@users.noreply.github.com>
2023-11-13 11:49:09 -0300

    13/11

--- pescaria.js
+++ pescaria.js
@@ -3,4 +3,20 @@ const resp = document.querySelector("h4")
 
 //ouvinte
 
-fmr.addEventListener(
+fmr.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const pessoas = Number(frm.inNumero.value)
+    const peixe = Number(frm.inPeixe.value)
+    let resultado
+        if(peixe > pessoas) {
+            resultado = (pessoas * 20) + ((peixe - pessoas) * 12)
+        } else {
+            resultado = pessoas * 20
+        }
+
+        resp.innerText = `N° de pessoas: ${pessoas}\n
+                          N° de peixes: ${peixe}\n
+                          Pagar: R$ ${resultado.toFixed(2)}`
+}
+)
